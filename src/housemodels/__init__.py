f28c68305781d07a25a949276b7977be2e2b1f3b
leonora31 <l.m.whitenburgh@liverpool.ac.uk>
2023-09-21 18:31:24 +0100

    Update __init__.py

--- src/housemodels/__init__.py
+++ src/housemodels/__init__.py
@@ -1,1 +1,16 @@
-package_var = "This is a package level variable"
+# Imports to expose classes for easier access when package is imported
+from .decision_tree_housing import DecisionTreeModel
+from .linear_regression_housing import LinearRegressionModel
+from .xg_boost_housing import XGBoostModel
+from .light_gbm_housing import LightGBMModel
+from .rnn_housing import RNNModel
+
+
+# Expose enum for external use
+__all__ = [
+    "DecisionTreeModel",
+    "LinearRegressionModel",
+    "XGBoostModel",
+    'LightGBMModel',
+    "RNNModel"
+]
